64cc03a9297f89167a2aebf3a440361ec3fc78ee
alvastern <alva.stern02@gmail.com>
2026-05-05 10:58:30 +0200

    Skapade funktioner för filtrering av kurser

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,36 +1,54 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Course, CourseService } from '../services/course';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-// En komponent som visar listan av kurser
+// Komponent som visar en lista av kurser
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 
-// Klass som implementerar OnInit och hämtar kurserna från CourseService
+// Interface för en kurs och en service som hämtar info från JSON
 export class Home implements OnInit {
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  searchTerm: string = '';
 
-  // Constructor som hämtar CourseService
-  constructor(private courseService: CourseService, 
+  // Constructor som implementerar courseService och ChangeDetectorRef
+  constructor(
+    private courseService: CourseService,
     private cdr: ChangeDetectorRef
   ) {}
 
-  // OnInit som hämtar kurserna
+  // OnInit som hämtar kurser + uppdaterar gränssnittet
   ngOnInit(): void {
-  console.log('ngOnInit körs');
+    this.courseService.getCourses().subscribe({
+      next: (data) => {
+        this.courses = data;
+        this.filteredCourses = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Fel vid hämtning:', err);
+      }
+    });
+  }
 
-  this.courseService.getCourses().subscribe({
-    next: (data) => {
-      this.courses = data;
-      this.cdr.detectChanges();
-    },
-    error: (err) => {
-      console.error('Fel vid hämtning:', err);
-    }
-  });
-}
+  // Funktion som filtrerar kurser baserat på användarens sökterm
+  filterCourses(): void {
+    this.filteredCourses = this.courses.filter(course =>
+      course.code.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      course.coursename.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  // Funktion som sorterar kurser baserat på det valda fältet
+  sortCourses(field: keyof Course): void {
+    this.filteredCourses.sort((a, b) =>
+      a[field].localeCompare(b[field])
+    );
+  }
 }
