860e8b21dadc9e0fe589578337d4a731c1099ecc
alvastern <alva.stern02@gmail.com>
2026-05-05 09:55:31 +0200

    Felsökte och löste funktionalitet

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Course, CourseService } from '../services/course';
+import { CommonModule } from '@angular/common';
 
+// En komponent som visar listan av kurser
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
+
+// Klass som implementerar OnInit och hämtar kurserna från CourseService
 export class Home implements OnInit {
   courses: Course[] = [];
 
-  constructor(private courseService: CourseService) {}
+  // Constructor som hämtar CourseService
+  constructor(private courseService: CourseService, 
+    private cdr: ChangeDetectorRef
+  ) {}
 
+  // OnInit som hämtar kurserna
   ngOnInit(): void {
-    this.courseService.getCourses().subscribe((data) => {
+  console.log('ngOnInit körs');
+
+  this.courseService.getCourses().subscribe({
+    next: (data) => {
       this.courses = data;
-    });
-  }
+      this.cdr.detectChanges();
+    },
+    error: (err) => {
+      console.error('Fel vid hämtning:', err);
+    }
+  });
+}
 }
--- src/app/services/course.ts
+++ src/app/services/course.ts
@@ -1,7 +1,9 @@
+// Imports av HttpClient 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// Ett interface som definierar en kurs
 export interface Course {
   coursename: string;
   code: string;
@@ -9,15 +11,19 @@ export interface Course {
   syllabus: string;
 }
 
+// En service som hämtar info från JSON
 @Injectable({
   providedIn: 'root',
 })
 
+// Ett interface som definierar en kurs, en service och hämtar info från JSON
 export class CourseService {
-  private apiURL = '/ramschema.json';
+  private apiURL = 'ramschema.json';
 
+  // Använder HttpClient för att hämta data från JSON
   constructor(private http: HttpClient) {}
 
+  // En metod som returnerar en Observable av Course array
   getCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(this.apiURL);
   }
